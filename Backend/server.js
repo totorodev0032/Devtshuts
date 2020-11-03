7dc2c1168ee8fad9c29a6d8a35072ae6db27bffd
nagendra0032 <nagendrakumar0032@gmail.com>
2020-11-03 18:56:35 +0530

    frontend backend , product list page,done

---
+++ Backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv.config()
+
+const products = require('./data/products');
+
+app.get('/', (req, res) => {
+    res.send('API is running');
+})
+
+app.get('/api/products', (req,res)=> {
+    res.json(products);
+})
+
+app.get('/api/products/:id', (req,res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.send(product);
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=> {console.log(`server running in ${process.env.NODE_ENV} at port ${PORT}`)})
