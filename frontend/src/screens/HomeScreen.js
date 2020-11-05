48642b160cda24ac51da0b5d939aba432dd4ab9f
nagendra0032 <nagendrakumar0032@gmail.com>
2020-11-05 19:13:47 +0530

    minor changes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -20,7 +20,7 @@ const HomeScreen = () => {
     }, [])
     return (
         <>
-            <h1>Latest Products</h1>
+            <h2 style = {{marginTop:'10px', marginBottom:'10px'}}>Dev T-Shirt</h2>
             <Row>
                 {products.map((product) => 
                     <Col key = {product._id} sm = {12} md = {6} lg = {4} xl = {3} >
