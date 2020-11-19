2477882397c6f27fdd5caf8eb705d883ae39cdb6
nagendra0032 <nagendrakumar0032@gmail.com>
2020-11-19 22:29:07 +0530

    custom error handling done

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,30 +1,26 @@
-
 const asyncHandler = require('express-async-handler');
 const Product = require('../models/productModel');
 
 // @desc     Fetch all Products
 // @route    GET /api/products
 // @access   Public
-const getProducts = asyncHandler(async(req,res) => {
-    const products = await Product.find({});
-    res.json(products);
-
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
 });
-
 
 // @desc     Fetch single products
 // @route    GET /api/products/:id
 // @access   Public
-const getProductById = asyncHandler(async(req,res) => {
-    const product = await Product.findById(req.params.id)
-    
-    if(product) {
-        res.json(product)
-    } else {
-        res.status(404).json({message: 'Product not found'})
-    }
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
 
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
 });
 
-module.exports = {getProducts, getProductById};
-
+module.exports = { getProducts, getProductById };
--- Backend/server.js
+++ Backend/server.js
@@ -2,23 +2,26 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 const colors = require('colors');
-const connectDB  = require('./config/db');
+const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
-dotenv.config()
+dotenv.config();
 
 connectDB();
 
-
-
 app.get('/', (req, res) => {
-    res.send('API is running');
+  res.send('API is running');
 });
 
 app.use('/api/products', productRoutes);
 
+app.use(notFound);
 
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> {console.log(`server running in ${process.env.NODE_ENV} at port ${PORT}`)})
+app.listen(PORT, () => {
+  console.log(`server running in ${process.env.NODE_ENV} at port ${PORT}`);
+});
